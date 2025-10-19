5fb68b5bb27345205131eac31ce31e841d8fab2f
Tirthik Sengupta <tirthik.sengupta@gmail.com>
2025-10-19 14:07:36 +0530

    Update

---
+++ Contest5 (Div. 2)/✅ A_Candies_for_Nephews.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+void solve()
+{
+    int n;
+    cin >> n;
+    if (n % 3 == 0)
+    {
+        cout << 0 << endl;
+        return;
+    }
+    cout << 3 - (n % 3) << endl;
+}
+int main() 
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t = 1;
+    cin >> t;
+    while (t--) 
+    {
+        solve();
+    }
+}
